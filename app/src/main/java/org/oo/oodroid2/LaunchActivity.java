4a2f4f6a0b1bc2e8c6bd83dbd0e39e7ee96a0a0e
entalent <1512503696@qq.com>
2015-03-07 23:17:06 +0800

    optimized UI added animation on launch screen

--- app/src/main/java/org/oo/oodroid2/LaunchActivity.java
+++ app/src/main/java/org/oo/oodroid2/LaunchActivity.java
@@ -4,20 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
 public class LaunchActivity extends Activity{
 
-    private final static int LAUNCH_ACTIVITY_DELAY = 3000;
+    private final static int LAUNCH_ACTIVITY_DELAY = 1500;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        View logo = findViewById(R.id.logo);
+        Animation anim = new ScaleAnimation(0.0f, 10.0f, 0.0f, 10.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        anim.setDuration(1510);
+        logo.setAnimation(anim);
+        anim.startNow();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(LaunchActivity.this,OoDroidActivity.class));
+                overridePendingTransition(R.anim.startanim, R.anim.exitanim);
                 finish();
             }
         },LAUNCH_ACTIVITY_DELAY);
